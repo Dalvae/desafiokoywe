482888d3f01b62749973017e8dac37dec05d0deb
Dalvae <diego.alvarez.e@ug.uchile.cl>
2025-02-07 17:12:03 -0300

    New version of nestjs

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -6,10 +6,9 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   const config = new DocumentBuilder()
-    .setTitle('Your API Title')
+    .setTitle('Desafio Koywe')
     .setDescription('API description')
     .setVersion('1.0')
-    .addTag('your-tag')
     .build();
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
